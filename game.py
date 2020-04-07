c4f1afe65750d083dea1ac96947ba28e43dbe58e
Mikhail Koval <mike.ry@mail.ru>
2020-04-07 09:02:14 +0300

    create game,py

---
+++ game.py
@@ -0,0 +1,46 @@
+import pygame
+import sys
+
+# COLORS
+LIGHT_BLUE = (0, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PINK = (255, 100, 180)
+ORANGE = (255, 100, 10)
+YELLOW = (255, 255, 0)
+# PARAMETERS
+WIDTH = 800
+HEIGHT = 600
+FPS = 60
+
+class main_window():
+    def __init__(self):
+        pygame.init()
+        self.height = 400 
+        self.width = 600
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.update()
+        self.objects = []
+        self.cycle()
+
+    def update(self):
+        #пока просто для наполненности написал:
+        pygame.draw.rect(self.window, YELLOW, (0, 0, 600, 200))
+        pygame.draw.rect(self.window, LIGHT_BLUE, (0, 200, 600, 400))
+        pygame.draw.lines(self.window, WHITE, False, [[x * x //50, x] for x in range(0, 600)])
+        pygame.display.update()
+        #for obj in self.objects:
+        #    obj.update()
+
+    def cycle(self):
+        clock = pygame.time.Clock()
+        while True:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            clock.tick(FPS)
+
+main_window()
--- map.py
+++ map.py
@@ -7,6 +7,6 @@ import random
 # честно, не очень понимаю, что именно этот класс будет делать
 class map:
     def __init__(self):
-        reflection = random.randint(0, gamesettings.existsReflection)
-        wind = random.randint(0, gamesettings.maxWind)
+        self.reflection = random.randint(0, gamesettings.existsReflection)
+        self.wind = random.randint(0, gamesettings.maxWind)
         # как-то выбирается или генерится рельеф
