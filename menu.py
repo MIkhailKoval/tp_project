e33865e83fa4b7c1cdfb060a19f9a91bdc25e7a1
makarovvladimir <makarov.vladimir@phystech.edu>
2020-04-27 19:24:51 +0300

    interface of menu graphics

--- menu.py
+++ menu.py
@@ -1,20 +1,46 @@
 from state import State, Game
-from abc import ABC
+from abc import ABC, abstractmethod
+from menu import menu_graphics
+import pygame
+import sys
 
 
 class Menu(State, ABC):
-    def handle(self):
+    options: 'List[str]'
+
+    def __init__(self):
         pass
 
+    def handle(self):
+        menu_graphics(self.options)
 
-class MainMenu(Menu):
+    @abstractmethod
+    def enter(self):
+        pass
+
+    @abstractmethod
+    def go_up(self):
+        pass
+
+    @abstractmethod
+    def go_down(self):
+        pass
+
+    def quit(self):
+        # pylint: disable=no-member
+        pygame.quit()
+        # pylint: enable=no-member
+        sys.exit()
+
+
+class MainMenu(Menu, ABC):
     def handle(self):
         print("MainMenu handles request")
         print("MainMenu wants to change the state of the context.")
         self.context.transition_to(Game())
 
 
-class PauseMenu(Menu):
+class PauseMenu(Menu, ABC):
     def handle(self):
         print("PauseMenu handles request")
         print("PauseMenu wants to change the state of the context.")
---
+++ menugraphics.py
@@ -0,0 +1,14 @@
+class menu_graphics:
+    def __init__(self, options: 'List[str]'):
+        # первая отрисовка экрана меню
+        pass
+
+    def redraw(self, options: 'List[str]'):
+        # перерисовка меню с новыми пунктами (вызывается при переходе из одного меню в другое)
+        pass
+
+    def select(self, name: str):
+        pass
+
+    def deselect(self, name: str):
+        pass
